8834efa20c436e332d7d0d3d6ff88c39ae2b3dff
Nicolás Rougier <nico@innovid.com>
2021-06-23 00:16:05 -0300

    Agrega una primera versión de un ejemplo

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import '@gcba/obelisco/dist/obelisco.css'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />
+}
+export default MyApp
