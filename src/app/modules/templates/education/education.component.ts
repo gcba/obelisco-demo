ce15eea6793c89f670b3540a8034d55c5c233d62
Jesus <jesus.fy.1997@gmail.com>
2023-12-14 16:39:55 -0300

    fix: ngx obelisco

--- src/app/modules/templates/education/education.component.ts
+++ src/app/modules/templates/education/education.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ONavbarModule } from '@gcba/ngx-obelisco/navbar';
 import { OFooterComponent } from '@gcba/ngx-obelisco/footer';
+import { OCardModule } from '@gcba/ngx-obelisco/card';
+
 import {
   TEMPLATES_ROUTES,
   TEMPLATES_ROUTES_SEARCHBARITEMS,
@@ -10,7 +12,7 @@ import {
 @Component({
   selector: 'app-education',
   standalone: true,
-  imports: [CommonModule, ONavbarModule, OFooterComponent],
+  imports: [CommonModule, ONavbarModule, OFooterComponent, OCardModule],
   templateUrl: './education.component.html',
   styleUrls: ['./education.component.scss'],
 })
