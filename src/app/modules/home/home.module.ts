cc0c861a7fac5dafc8322a3e2b7589cedeb40d0f
Jesus <jesus.fy.1997@gmail.com>
2023-12-21 15:53:55 -0300

    fix: delete clipboard and prism

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,18 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { PrismDirective } from 'src/app/shared/directives/prism/prism.directive';
 import { HighlightModule } from 'ngx-highlightjs';
 import { ONavbarModule } from '@gcba/ngx-obelisco/navbar';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    PrismDirective,
-    HighlightModule,
-    ONavbarModule,
-  ],
+  imports: [CommonModule, HomeRoutingModule, HighlightModule, ONavbarModule],
 })
 export class HomeModule {}
